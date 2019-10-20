0fe782df52a586fe32ac1cf0ff457e4cbaef9823
Christian <chris.busch.22@gmail.com>
2019-10-20 19:09:51 +0200

    Spinnaker functions added to Camera.

--- KISS_camera/camera/Camera.py
+++ KISS_camera/camera/Camera.py
@@ -4,8 +4,6 @@ from ..Image import Image
 
 class Camera:
     # settings
-    color_processing_algorithm = PySpin.HQ_LINEAR
-    pixel_format = PySpin.PixelFormat_RGB8
 
     # status variable
     status_acquisition = False
@@ -18,25 +16,17 @@ class Camera:
         system = None
     ):
         # create system instance if not provided
-        if( system is None ):
+        if(system is None):
             self.system = System()
         
         # get camera handle
-        self.camera = None
-        if(serial_number is not None):
-            self.camera = self.system.get_camera_by_serial_number(serial_number)
-            if(self.camera is None):
-                raise ValueError( 'serial_number: not found' )
-        else:
-            self.camera = self.system.get_camera_by_index(index)
-            if(self.camera is None):
-                raise ValueError( 'index: not found' )
+        self.camera = self.get_camera_handle(serial_number, index)
 
-        # retrieve TL device nodemap
+        # retrieve TL device node map
         self.nodemap_GetTLDevice = self.camera.GetTLDeviceNodeMap()
 
         # initialize camera
-        self.camera.Init()
+        self.init()
 
         # retrieve GenICam nodemap
         self.nodemap_GenICam = self.camera.GetNodeMap()
@@ -48,8 +38,8 @@ class Camera:
         self.set_pixel_format()
             
     def __del__( self ):
-        # end acquisition if still runing
-        if( self.status_acquisition == True ):
+        # end acquisition if still running
+        if( self.is_streaming() == True ):
             self.end_acquisition()
         
         # deinitialize camera
@@ -66,43 +56,56 @@ class Camera:
     
     def get_system(self):
         return self.system
-    
-    def begin_acquisition( self ):
-        self.camera.BeginAcquisition()
-        self.status_acquisition = True
 
-    def end_acquisition( self ):
-        self.camera.EndAcquisition()
-        self.status_acquisition = False
-    
-    def is_acquiring(self):
-        raise NotImplementedError
+    def get_camera_handle(
+        self,
+        serial_number,
+        index
+    ):
+        # check system
+        if(self.system is None):
+            return None
+
+        # get handle
+        camera = None
+        if(serial_number is not None):
+            camera = self.system.get_camera_by_serial_number(serial_number)
+            if(camera is None):
+                raise ValueError('serial_number: not found')
+        else:
+            camera = self.system.get_camera_by_index(index)
+            if(camera is None):
+                raise ValueError('index: not found')
         
-    def get_image( self ):
+        return camera
+
+    def get_image(self):
         try:
             # retrieve next received image
-            image_next = self.camera.GetNextImage()
+            image_next = self.get_next_image()
 
             # ensure image completion
-            if( image_next.IsIncomplete() ):
-                print( "Image incomplete with image status %d ..." % image_next.GetImageStatus() )
+            if(image_next.is_incomplete()):
+                print("Image incomplete with image status %d ..." % image_next.get_image_status())
                 return False
             
             # converted image does not affect the camera buffer (no release required)
-            image = image_next.Convert(
-                self.pixel_format,
-                self.color_processing_algorithm
+            pixel_format = PySpin.PixelFormat_RGB8
+            color_processing_algorithm = PySpin.HQ_LINEAR
+            image = image_next.convert(
+                format = pixel_format,
+                algorithm = color_processing_algorithm
             )
 
             # release image
-            image_next.Release()
+            image_next.release()
         except Exception as e:
-            print( e )
+            print(e)
             return False 
         
-        return Image(image)
+        return image
 
-    def set_acquisition_mode( self, acquisition_mode ):
+    def set_acquisition_mode(self, acquisition_mode):
         # In order to access the node entries, they have to be casted to a pointer type (CEnumerationPtr here)
         node_acquisition_mode = PySpin.CEnumerationPtr( self.nodemap_GenICam.GetNode( "AcquisitionMode" ) )
         if not PySpin.IsAvailable( node_acquisition_mode ) or not PySpin.IsWritable( node_acquisition_mode ):
@@ -162,8 +165,95 @@ class Camera:
         acquisition_framerate = node_acquisition_framerate.GetValue()
 
         return acquisition_framerate
-    
-    def __error( self, exception ):
-        #except PySpin.SpinnakerException as exception:
-        print( "Error: %s" % exception )
-        return False
+
+    ## spinnaker
+    def init(self):
+        self.camera.Init()
+
+    def de_init(self):
+        self.camera.DeInit()
+
+    def is_initialized(self):
+        return self.camera.IsInitialized()
+
+    def is_valid(self):
+        return self.camera.IsValid()
+
+    def get_node_map(self):
+        return self.camera.GetNodeMap()
+
+    def get_TL_device_node_map(self):
+        return self.camera.GetTLDeviceNodeMap()
+
+    def get_TL_stream_node_map(self):
+        return self.camera.GetTLStreamNodeMap()
+
+    def get_access_mode(self):
+        return self.camera.GetAccessMode()
+
+    def read_port(self, i_address, p_buffer, i_size):
+        self.camera.ReadPort(i_address, p_buffer, i_size)
+
+    def write_port(self, i_address, p_buffer, i_size):
+        self.camera.WritePort(i_address, p_buffer, i_size)
+
+    def begin_acquisition(self):
+        self.camera.BeginAcquisition()
+
+    def end_acquisition(self):
+        self.camera.EndAcquisition()
+
+    def get_buffer_ownership(self):
+        return self.camera.GetBufferOwnership()
+
+    def set_buffer_ownership(self, mode):
+        self.camera.SetBufferOwnership(mode)
+
+    def get_user_buffer_count(self): 
+        return self.camera.GetUserBufferCount()
+
+    def get_user_buffer_size(self): 
+        return self.camera.GetUserBufferSize()
+
+    def get_user_buffer_total_size(self): 
+        return self.camera.GetUserBufferTotalSize()
+
+    def set_user_buffers(self, p_mem_buffers, total_size, buffer_count, buffer_size): 
+        #self.camera.SetUserBuffers(p_mem_buffers, total_size)
+        #self.camera.SetUserBuffers(pp_mem_buffers, buffer_count, buffer_size)
+        raise NotImplementedError
+
+    def get_next_image(
+        self,
+        grab_timeout = PySpin.EVENT_TIMEOUT_INFINITE,
+        stream_ID = 0
+    ): 
+        image = self.camera.GetNextImage(grab_timeout, stream_ID)
+        return Image(image)
+
+    def get_unique_id(self): 
+        return self.camera.GetUniqueID()
+
+    def is_streaming(self): 
+        return self.camera.IsStreaming()
+
+    def get_gui_xml(self):
+        return self.camera.GetGuiXml()
+
+    def register_event(self, evt_to_register, event_name):
+        self.camera.RegisterEvent(evt_to_register)
+
+    def unregister_event(self, evt_to_unregister): 
+        self.camera.UnregisterEvent(evt_to_unregister)
+
+    def get_num_images_in_use(self): 
+        return self.camera.GetNumImagesInUse()
+
+    def get_num_data_streams(self): 
+        return self.camera.GetNumDataStreams()
+
+    def discover_max_packet_size(self): 
+        return self.camera.DiscoverMaxPacketSize()
+
+    def force_ip(self): 
+        self.camera.ForceIP()
